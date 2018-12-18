ea84cb9f2f5d7a25f372990da46a70ac035b01b7
15833078586 <15833078586@qq.com>
2018-12-18 23:09:47 +0800

    12.18:23:09

---
+++ Exam/Controllers/BookController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Exam.Models;
+using PagedList.Mvc;
+
+namespace Exam.Controllers
+{
+    /// <summary>
+    /// 教材管理
+    /// </summary>
+    public class BookController : Controller
+    {
+        ExamSysEntities ef = new ExamSysEntities();
+        // GET: Book
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
