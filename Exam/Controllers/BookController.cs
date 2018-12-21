ccb01476e4ce8d03d872230905712c01a9cad5b3
15833078586 <15833078586@qq.com>
2018-12-21 11:45:38 +0800

    12/21 11:45

--- Exam/Controllers/BookController.cs
+++ Exam/Controllers/BookController.cs
@@ -8,6 +8,7 @@ using PagedList.Mvc;
 using Exam.Controllers;
 using Exam.Models.ViewModels;
 using PagedList;
+using System.Data.Entity;
 
 namespace Exam.Controllers
 {
@@ -17,6 +18,7 @@ namespace Exam.Controllers
     public class BookController : Controller
     {
         ExamSysEntities1 ef = new ExamSysEntities1();
+        
         // GET: Book
         public ActionResult Index()
         {
@@ -35,25 +37,109 @@ namespace Exam.Controllers
         {
 
             int ye = Convert.ToInt32(page)==0?1: Convert.ToInt32(page);
+            List<TextBooks> li = Shou();
+            
+            int CountYe = li.Count % 10 > 0 ? (li.Count / 10) + 1 : li.Count / 10;
+            var Tili = new {Li=li.Skip((ye-1)*10).Take(10), Ye=CountYe,Hang= page };
+             return Json(Tili, JsonRequestBehavior.AllowGet);
+        }
+
+       /// <summary>
+       /// 查询返回分页 可以根据教材名称以及年级
+       /// </summary>
+       /// <param name="page"></param>
+       /// <param name="GradeID"></param>
+       /// <param name="TiN"></param>
+       /// <returns></returns>
+        [HttpPost]
+        public ActionResult Select(string page, string GradeID, string TiN)
+        {
+            int ye = Convert.ToInt32(page) == 0 ? 1 : Convert.ToInt32(page);
+            List<TextBooks> li = Shou();
+            List<TextBooks> pagedList = null;
+            int GradID = Convert.ToInt32(GradeID);
+            Grade GrNa = ef.Grades.FirstOrDefault(x => x.GradeID == GradID);
+            if (!string.IsNullOrEmpty(TiN) && !string.IsNullOrEmpty(GradeID))
+            {
+                if (GradID >= 1)
+                {
+                    pagedList = li.Where(x => x.BookName == TiN && x.GradeName == GrNa.GradeName).Skip((ye - 1) * 10).Take(10).ToList();
+                }else
+                    pagedList = li.Where(x => x.BookName == TiN).ToList();
+
+            }
+            else if (string.IsNullOrEmpty(TiN) && GradID>=1)
+            {
+                pagedList = li.Where(x =>x.GradeName == GrNa.GradeName).ToList();
+            }
+            else if(string.IsNullOrEmpty(GradeID))
+            {
+                pagedList = li.Where(x => x.BookName == TiN ).ToList();
+            }
+            else
+            {
+                pagedList= li.ToList();
+            }
+            int CountYe = pagedList.Count % 10 > 0 ? (pagedList.Count / 10) + 1 : pagedList.Count / 10;
+            var Tili = new { Li = pagedList.Skip((ye-1)*10).Take(10).ToList(), Ye = CountYe, Hang = page };
+            return Json(Tili, JsonRequestBehavior.AllowGet);
+        }
+        /// <summary>
+        /// 添加对象
+        /// </summary>
+        /// <param name="te"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult ADD(TextBook te)
+        {
+            ef.Entry(te).State= EntityState.Added;
+                if (ef.SaveChanges() > 0)
+                {
+                    return Json(Shou(), JsonRequestBehavior.AllowGet);
+                }
+                else
+                    return Content("失败");
+            
+        }
+        /// <summary>
+        /// 清除教材
+        /// </summary>
+        /// <param name="te"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Clear(TextBook te)
+        {
+            string name = te.BookName;
+            ef.TextBooks.Remove(ef.TextBooks.FirstOrDefault(x =>x.BookName==te.BookName));
+            if (ef.SaveChanges() > 0)
+            {
+                return Json(Shou(), JsonRequestBehavior.AllowGet);
+            }
+            else
+                return Content("失败");
+        }
+        [HttpPost]
+        public ActionResult Update(TextBook te)
+        {
+            ef.Entry(te).State = EntityState.Modified;
+            if (ef.SaveChanges() > 0)
+            {
+                return Json("成功",JsonRequestBehavior.AllowGet);
+            }
+            return Content("失败");
+        }
+        public List<TextBooks> Shou()
+        {
             List<TextBooks> li = new List<TextBooks>();
             foreach (var item in ef.TextBooks.ToList())
             {
                 TextBooks Book = new TextBooks();
                 Book.BookID = item.BookID;
-                if (!string.IsNullOrEmpty(TiN))
-                    Book.BookName =TiN;
-                else
-                    Book.BookName = item.BookName;
-                if (!string.IsNullOrEmpty(GradeID))
-                    Book.GradeName = ef.Grades.FirstOrDefault(x => x.GradeID == item.GradeID && x.GradeID == int.Parse(GradeID) ? item.GradeID == int.Parse(GradeID) : x.GradeID == item.GradeID).GradeName;
-                else
-                    Book.GradeName = ef.Grades.FirstOrDefault(x => x.GradeID == item.GradeID).GradeName;
+                Book.BookName = item.BookName;
+                Book.GradeName = ef.Grades.FirstOrDefault(x => x.GradeID == item.GradeID).GradeName;
                 li.Add(Book);
-             }
-             List<TextBooks> pagedList = li.Skip((ye - 1) * 10).Take(10).ToList();
-             int CountYe=li.Count % 10 > 0 ? (li.Count / 10) + 1 : li.Count / 10;
-             var Tili = new {Li=pagedList,Ye=CountYe,Hang= 10 };
-             return Json(Tili, JsonRequestBehavior.AllowGet);
+            }
+            return li;
         }
     }
 }
