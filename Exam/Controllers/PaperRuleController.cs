85fa1746e36e45054b3267a2bf7e902f11240d3b
15833078586 <15833078586@qq.com>
2019-01-02 20:55:03 +0800

    1/2

---
+++ Exam/Controllers/PaperRuleController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Exam.Models;
+using Exam.Models.ViewModels;
+using System.Web.Mvc;
+
+namespace Exam.Controllers
+{
+    public class PaperRuleController : Controller
+    {
+        // GET: PaperRule
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
